85404e0547c8e9c95f5093bd4f41fb42edcb799e
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:54:08 +0800

    Update main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,19 +1,19 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_id
+resource "aws_instance" "web" {
+  ami = "ami-05576a079321f21f8"  # Use the correct AWS AMI
   instance_type = var.instance_type
   monitoring    = true
 
   tags = {
-    Name = "CostOptimizationInstance"
+    Name = "aalimsee-WebServer"
   }
 
   user_data = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install -y stress fio python3
-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install -y stress fio python3
+  EOF
 }
 
-output "instance_id" {
-  value = aws_instance.example.id
+output "ec2_instance_id" {
+  value = aws_instance.web.id
 }
