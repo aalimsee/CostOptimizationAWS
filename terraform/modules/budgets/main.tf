bc00aabc618dc4b233f2ffd649e289b9a0dfa0e9
aalimsee <aaron.limse@hotmail.com>
2025-01-11 21:42:14 +0800

    Update main.tf

--- terraform/modules/budgets/main.tf
+++ terraform/modules/budgets/main.tf
@@ -16,3 +16,22 @@ resource "aws_budgets_budget" "monthly_budget" {
     }
   }
 }
+
+resource "aws_budgets_budget" "example" {
+  name          = var.budget_name
+  budget_type   = "COST"
+  limit_amount  = var.budget_limit
+  limit_unit    = "USD"
+  time_unit     = "MONTHLY"
+
+  notification {
+    comparison_operator = "GREATER_THAN"
+    notification_type   = "ACTUAL"
+    threshold           = 80
+
+    subscriber {
+      subscription_type = "EMAIL"
+      address           = var.notification_email
+    }
+  }
+}
