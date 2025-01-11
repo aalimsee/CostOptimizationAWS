aa7fc6da1491f0c622af49fc46314a0ceecbf71c
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:16:26 +0800

    Update main.tf

--- terraform/modules/budgets/main.tf
+++ terraform/modules/budgets/main.tf
@@ -1,19 +1,27 @@
 resource "aws_budgets_budget" "monthly_budget" {
-  name              = var.budget_name
-  budget_type       = "COST"
-  limit_amount      = var.budget_limit
-  limit_unit        = "USD"
-  time_unit         = "MONTHLY"
+  name          = var.budget_name
+  budget_type   = "COST"
+  limit_amount  = var.budget_limit
+  limit_unit    = "USD"
+  time_unit     = "MONTHLY"
 
   notification {
     comparison_operator = "GREATER_THAN"
-    notification_type   = "FORECASTED"
-    threshold           = 90
-    threshold_type      = "PERCENTAGE" # Add this line
+    notification_type   = "ACTUAL"
+    threshold           = 80
+    threshold_type      = "PERCENTAGE"
+  }
+}
 
-    subscriber {
-      subscription_type = "EMAIL"
-      address           = var.notification_email
-    }
+resource "aws_budgets_budget_notification" "budget_notification" {
+  budget_name = aws_budgets_budget.monthly_budget.name
+  comparison_operator = "GREATER_THAN"
+  threshold           = 80
+  threshold_type      = "PERCENTAGE"
+  notification_type   = "ACTUAL"
+
+  subscriber {
+    subscription_type = "EMAIL"
+    address           = var.notification_email
   }
 }
