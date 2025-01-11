110a08778563ee9300cc2414cff80c4019f9de47
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:22:35 +0800

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -13,3 +13,9 @@ resource "aws_s3_bucket" "reports" {
 output "bucket_name" {
   value = aws_s3_bucket.reports.bucket
 }
+
+module "s3" {
+  source     = "./modules/s3"
+  bucket_name = var.bucket_name  # Pass the bucket_name variable here
+  versioning  = true  # Or pass the appropriate value for versioning
+}
