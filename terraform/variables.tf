d2ac679e2bad790a2007fb88db2f4053e93f5921
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:20:49 +0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,3 +21,9 @@ variable "budget_limit" {
 variable "notification_email" {
   default     = "aaron.limse@hotmail.com"
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "aalimsee-s3-bucket" # Provide a default value or leave it empty to require input
+}
