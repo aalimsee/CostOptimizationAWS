5735035809a240841f92c2f8305bfe33521ade62
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:12:41 +0800

    Update main.tf
    remove 
    
    resource "aws_budgets_budget" "monthly_budget" {
      name          = var.budget_name
      budget_type   = "COST"
      limit_amount  = var.budget_limit
      limit_unit    = "USD"
      time_unit     = "MONTHLY"
    
      notification {
        comparison_operator = "GREATER_THAN"
        notification_type   = "ACTUAL"
        threshold           = 80
        threshold_type      = "PERCENTAGE"
      }
    
      subscriber {
        subscription_type = "EMAIL"
        address           = var.notification_email
      }
    }

--- terraform/modules/budgets/main.tf
+++ terraform/modules/budgets/main.tf
@@ -17,23 +17,3 @@ resource "aws_budgets_budget" "monthly_budget" {
     }
   }
 }
-
-resource "aws_budgets_budget" "example" {
-  name          = var.budget_name
-  budget_type   = "COST"
-  limit_amount  = var.budget_limit
-  limit_unit    = "USD"
-  time_unit     = "MONTHLY"
-
-  notification {
-    comparison_operator = "GREATER_THAN"
-    notification_type   = "ACTUAL"
-    threshold           = 80
-    threshold_type      = "PERCENTAGE"
-  }
-
-  subscriber {
-    subscription_type = "EMAIL"
-    address           = var.notification_email
-  }
-}
