ab1f350a542ffdecbc8b4586e834b779466dfd20
aalimsee <aaron.limse@hotmail.com>
2025-01-11 21:59:34 +0800

    Update variables.tf

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -1,11 +1,1 @@
-variable "instance_type" {
-  default     = "t2.micro"
-}
 
-variable "ami_id" {
-  default     = "ami-05576a079321f21f8"
-}
-
-variable "s3_bucket_name" {
-  default     = "cost-optimization-reports"
-}
