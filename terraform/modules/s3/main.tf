8b7f7344ae20880d8a2d7b43d6873e26bf0f01d6
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:56:19 +0800

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,20 +1,14 @@
-resource "aws_s3_bucket" "reports" {
+resource "aws_s3_bucket" "cost_reports" {
   bucket = var.bucket_name
   versioning {
     enabled = var.versioning
   }
 
   tags = {
-    Name = "CostOptimizationReports"
+    Name = "aalimsee-CostOptimizationReports"
   }
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.reports.bucket
-}
-
-module "s3" {
-  source     = "./modules/s3"
-  bucket_name = var.bucket_name  # Pass the bucket_name variable here
-  versioning  = true  # Or pass the appropriate value for versioning
+output "s3_bucket_name" {
+  value = aws_s3_bucket.cost_reports.bucket
 }
