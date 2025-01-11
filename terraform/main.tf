0ef1134ec14d1a28df2a92e0cb943266584ca839
aalimsee <aaron.limse@hotmail.com>
2025-01-11 22:37:50 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,17 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-# EC2 Instance
-module "ec2" {
-  source       = "./modules/ec2"
-  instance_type = var.instance_type
-  ami_id        = var.ami_id
+module "ec2_instance" {
+  source = "./modules/ec2"
 }
 
-# S3 Bucket for Reports
-module "s3" {
-  source         = "./modules/s3"
-#  bucket_name    = var.s3_bucket_name
-#  versioning     = true
+module "s3_bucket" {
+  source = "./modules/s3"
 }
 
-# AWS Budgets
-module "budgets" {
-  source           = "./modules/budgets"
-  budget_name      = var.budget_name
-  budget_limit     = var.budget_limit
-  notification_email = var.notification_email
-}
-
-output "ec2_instance_id" {
-  value = module.ec2.instance_id
-}
-
-output "s3_bucket_name" {
-  value = module.s3.bucket_name
+module "cost_budget" {
+  source = "./modules/budgets"
+  budget_name = "MonthlyCostBudget"
+  limit       = var.budget_limit
 }
